8f663c9a19352c0f8f7f5bd53179527009517c57
Yun Seob Lee <fhzotxldj@gmail.com>
2022-10-27 15:52:19 +0900

    deploy

---
+++ src/i18n.mjs
@@ -0,0 +1,23 @@
+// import axios from "axios";
+
+// // let countryName
+
+// // (async () => {
+// //   countryName = axios
+// //   .get("https://ipapi.co/json/")
+// //   .then((response) => response.data.country_name)
+// //   .catch((err) => console.log(err));
+// // })();
+
+// // export default (async () => {
+// //   const url = 'https://jsonplaceholder.typicode.com/users';
+// //   const response = await fetch(url);
+// //   users = await response.json();
+// // })();
+
+// // export default { countryName };
+// const url = 'https://jsonplaceholder.typicode.com/users';
+// const response = await fetch(url);
+// let users = await response.json();
+
+// export { users };
